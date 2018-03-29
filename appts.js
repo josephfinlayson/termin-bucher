00ebc86e0030521217bc4d3d043728fdac30b4e6
Joseph Finlayson <joseph.finlayson@gmail.com>
2018-03-29 11:01:08 +0200

    reformat with prettier

--- appts.js
+++ appts.js
@@ -1,12 +1,12 @@
-import renderEmail from './email'
+import renderEmail from "./email";
 import apptAgreement from "./page-objects/appointment-agreement";
-import {getApptDetails} from './formatter/get-appt-info'
-import _ from 'lodash';
-import {api_key, domain, emails} from './api-key'
-import cheerio from 'cheerio';
-import fetch from 'node-fetch';
+import { getApptDetails } from "./formatter/get-appt-info";
+import _ from "lodash";
+import { api_key, domain, emails } from "./api-key";
+import cheerio from "cheerio";
+import fetch from "node-fetch";
 
-const mailgun = require('mailgun-js')({
+const mailgun = require("mailgun-js")({
   apiKey: api_key,
   domain: domain
 });
@@ -14,32 +14,37 @@ const mailgun = require('mailgun-js')({
 function checkForAppts() {
   return fetch(apptAgreement.url)
     .then(response => {
-      return response.text()
-    }).then((html) => {
-      const $ = cheerio.load(html)
-      const appts = $(apptAgreement.appointmentTable).find(apptAgreement.bookableAppt);
+      return response.text();
+    })
+    .then(html => {
+      const $ = cheerio.load(html);
+      const appts = $(apptAgreement.appointmentTable).find(
+        apptAgreement.bookableAppt
+      );
 
       if (!appts.length) {
-        console.log('nothing found')
+        console.log("nothing found");
       } else {
-
-        const apptLinks = appts.map(_.partialRight(getApptDetails, $))
+        const apptLinks = appts.map(_.partialRight(getApptDetails, $));
 
         const data = {
-          from: 'Excited User <me@samples.mailgun.org>',
+          from: "Excited User <me@samples.mailgun.org>",
           to: emails,
-          subject: 'APPT FOUND',
+          subject: "APPT FOUND",
           html: renderEmail(apptLinks.toArray())
         };
 
-        mailgun.messages().send(data, function (error, body) {
+        mailgun.messages().send(data, function(error, body) {
           console.log(error, body);
         });
-        console.log('sent')
+        console.log("sent");
       }
-    }).catch(console.error)
-
+    })
+    .catch(console.error);
 }
 
-checkForAppts()
-setInterval(checkForAppts, 60 * 1000 * Math.floor((Math.random() + 1) * (10 - 1)) + 1)
+checkForAppts();
+setInterval(
+  checkForAppts,
+  60 * 1000 * Math.floor((Math.random() + 1) * (10 - 1)) + 1
+);

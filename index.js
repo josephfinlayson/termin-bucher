2672dc2a87ec19fc091b0980a6909f54cff839e3
Joseph Finlayson <joseph.finlayson@gmail.com>
2018-04-09 16:56:56 +0200

    restructuring endpoints to conform to cloudfront paramenters

--- index.js
+++ index.js
@@ -16,14 +16,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/hello', (req, res) => res.send('hello from cloudfront!'))
-app.get('/health', (req, res) => res.send('Healthy!'))
 
-app.get('/', (req, res) => res.send('Hello new !'))
-
-
-app.post('/email', (req, res) => {
-
+app.post('/api/email', (req, res) => {
 
     if (validator.validate(req.body.email)) {
       knex.table('users').insert({email: req.body.email})
@@ -36,7 +30,7 @@ app.post('/email', (req, res) => {
 )
 
 
-app.get('/email', (req, res) => {
+app.get('/api/email', (req, res) => {
     if (validator.validate(req.body.email)) {
       knex.table('users').delete({email: req.body.email})
         .then(r => res.send(r))
@@ -49,16 +43,18 @@ app.get('/email', (req, res) => {
   }
 )
 
+const health =  (req, res) => {
+  knex.raw('select 1+1 as result')
+    .then(r => res.send(r))
+    .catch(() => {
+      res.status(500).send('Something broke!')
+    });
+}
 
-app.get('/db', (req, res) => {
-    knex.raw('select 1+1 as result')
-      .then(r => res.send(r))
-      .catch((err) => {
-        res.error(err, result);
-      });
-  }
-)
+app.get('/api/health', health)
+app.get('/health', health)
 
 const port = 3000
+
 app.listen(port, () => console.log(`Example app listening on  ${port}!`))
 

f86c2fc1a681a9d9c357b227aa85961fb8feb891
Joseph Finlayson <joseph.finlayson@gmail.com>
2018-04-09 10:39:13 +0200

    run migration

--- index.js
+++ index.js
@@ -7,17 +7,34 @@ const express = require('express')
 const app = express()
 import knex from './data/index'
 
+var validator = require("email-validator");
+
+
 require('dotenv').load();
 require("./appts");
 
 app.get('/health', (req, res) => res.send('Healthy!'))
 
 app.get('/', (req, res) => res.send('Hello new !'))
+
+
+app.post('/email', (req, res) => {
+    if (validator.validate(req.body.email)) {
+      knex.table('users').insert({email: req.body.email})
+        .then(r => res.send(r))
+        .catch((err) => {
+          res.error(err, result);
+        });
+    }
+  }
+)
+
+
 app.get('/db', (req, res) => {
     knex.raw('select 1+1 as result')
       .then(r => res.send(r))
       .catch((err) => {
-        res.send(err, result);
+        res.error(err, result);
       });
   }
 )

8fd7969329c863459a75ffdf7adfa7b0880ae6e5
Joseph Finlayson <joseph.finlayson@gmail.com>
2018-04-10 18:16:10 +0200

    Error handling

--- index.js
+++ index.js
@@ -3,58 +3,57 @@
  */
 "use strict";
 
-import express from 'express'
-import knex from './data/index'
+import express from "express";
+import knex from "./data/index";
 import * as validator from "email-validator";
-import cors from 'cors'
+import cors from "cors";
 
+import "./appts";
 
-import "./appts"
+const app = express();
 
-const app = express()
+app.use(cors());
+app.use(express.json());
 
-app.use(cors())
-app.use(express.json())
-
-
-app.post('/api/email', (req, res) => {
-
-    if (validator.validate(req.body.email)) {
-      knex.table('users').insert({email: req.body.email})
-        .then(r => res.send(r))
-        .catch((err) => {
-          res.send(err);
-        });
-    }
+app.post("/api/email", (req, res) => {
+  if (validator.validate(req.body.email)) {
+    knex
+      .table("users")
+      .insert({ email: req.body.email })
+      .then(r => res.send(r))
+      .catch(err => {
+        res.send(err);
+      });
   }
-)
+});
 
-
-app.get('/api/email', (req, res) => {
-    if (validator.validate(req.body.email)) {
-      knex.table('users').delete({email: req.body.email})
-        .then(r => res.send(r))
-        .catch((err) => {
-          console.error(err)
-          // res.
-
-        });
-    }
+app.get("/api/email", (req, res) => {
+  if (validator.validate(req.body.email)) {
+    knex
+      .table("users")
+      .delete({ email: req.body.email })
+      .then(res.send)
+      .catch(err => {
+        console.error(err);
+        res.status(500).send({err: "SOMETHING_BROKE"});
+      });
+  } else {
+    res.status(400).send({ err: "INVALID_EMAIL" });
   }
-)
+});
 
-const health =  (req, res) => {
-  knex.raw('select 1+1 as result')
+const health = (req, res) => {
+  knex
+    .raw("select 1+1 as result")
     .then(r => res.send(r))
     .catch(() => {
-      res.status(500).send('Something broke!')
+      res.status(500).send("Something broke!");
     });
-}
+};
 
-app.get('/api/health', health)
-app.get('/health', health)
+app.get("/api/health", health);
+app.get("/health", health);
 
-const port = 3000
+const port = 3000;
 
-app.listen(port, () => console.log(`Example app listening on  ${port}!`))
-
+app.listen(port, () => console.log(`Example app listening on  ${port}!`));

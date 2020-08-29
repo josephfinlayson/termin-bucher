f9fbe11aa7835bb6ca950a11a47fcbf4c4c99a0a
Joseph Finlayson <joseph.finlayson@gmail.com>
2020-08-29 12:12:18 +0200

    fix var ref

--- appts.js
+++ appts.js
@@ -52,16 +52,19 @@ function getEmailsRegisteredInLast7Days() {
 }
 
 async function checkForAppts() {
+    let $
     try {
         const browser = await puppeteer.launch({ headless: false });
         const page = await browser.newPage();
         await page.goto(apptAgreement.url, { waitUntil: 'networkidle2' });
         await page.goto("https://service.berlin.de/terminvereinbarung/termin/restart/?providerList=122210%2C122217%2C327316%2C122219%2C327312%2C122227%2C122231%2C327346%2C122243%2C327348%2C122252%2C329742%2C122260%2C329745%2C122262%2C329748%2C122254%2C329751%2C122271%2C327278%2C122273%2C122277%2C327276%2C122280%2C327294%2C122282%2C327290%2C122284%2C327292%2C122291%2C327270%2C122285%2C327266%2C122286%2C327264%2C122296%2C327268%2C150230%2C329760%2C122301%2C327282%2C122297%2C327286%2C122294%2C327284%2C122312%2C329763%2C122304%2C327330%2C122311%2C327334%2C122309%2C327332%2C122281%2C327352%2C122279%2C122276%2C327324%2C122274%2C327326%2C122267%2C329766%2C122246%2C327318%2C122251%2C327320%2C122257%2C122208%2C122226&requestList=120686&source=dldb")
         let html = await page.evaluate(() => document.body.innerHTML);
-        const $ = cheerio.load(html)
+        
+        $ = cheerio.load(html)
 
     } catch (e) {
         console.error(JSON.stringify(e))
+        return
 
     }
     const appts = getBookableAppointments($)

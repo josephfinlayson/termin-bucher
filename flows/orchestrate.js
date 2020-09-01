fd0fd2aab3ad7aee4faecfc49456d935258c7075
Joseph Finlayson <joseph.finlayson@gmail.com>
2020-09-01 17:31:29 +0200

    format

--- flows/orchestrate.js
+++ flows/orchestrate.js
@@ -5,44 +5,44 @@ import bookAppts from './book-appointments'
 import notify from './notify-users'
 import markUserAsFound from './mark-user-as-found'
 
-async function checkForAppts() {
-    const users = await getUsers()
-    if (users.length === 0) {
-        console.log(' No users')
-        return
-    }
+async function checkForAppts () {
+  const users = await getUsers()
+  if (users.length === 0) {
+    console.log(' No users')
+    return
+  }
 
-    const browser = await puppeteer.launch({ headless: process.env.NODE_ENV === 'production', args: ['--no-sandbox', '--disable-setuid-sandbox'] })
-    const page = await browser.newPage()
-    const appts = await getAppointments(page)
+  const browser = await puppeteer.launch({ headless: process.env.NODE_ENV === 'production', args: ['--no-sandbox', '--disable-setuid-sandbox'] })
+  const page = await browser.newPage()
+  const appts = await getAppointments(page)
 
-    if (appts.length === 0) {
-        console.log(' No appointments')
-        return
-    }
+  if (appts.length === 0) {
+    console.log(' No appointments')
+    return
+  }
 
-    console.log(appts)
-    const usersWithScreenShots = await users.map(async function (user, i) {
-        return { screenshot: await bookAppts(page, user, appts[i].link), user }
-    }).filter(userWithScreenshot => !!userWithScreenshot.screenshot)
+  console.log(appts)
+  const usersWithScreenShots = await users.map(async function (user, i) {
+    return { screenshot: await bookAppts(page, user, appts[i].link), user }
+  }).filter(userWithScreenshot => !!userWithScreenshot.screenshot)
 
-    usersWithScreenShots.forEach(user => markUserAsFound(user.user))
-    usersWithScreenShots.forEach(user => notify(user.user, user.screenshot))
+  usersWithScreenShots.forEach(user => markUserAsFound(user.user))
+  usersWithScreenShots.forEach(user => notify(user.user, user.screenshot))
 
-    browser.close()
+  browser.close()
 }
 
 checkForAppts();
 
-(function loop() {
-    const rand = Math.round(Math.random() * (300000 - 15000) + 1500)
-    console.log(
-        'next check scheduled for',
-        Math.round(rand / 1000 / 60),
-        'minutes'
-    )
-    setTimeout(function () {
-        checkForAppts()
-        loop()
-    }, rand)
+(function loop () {
+  const rand = Math.round(Math.random() * (300000 - 15000) + 1500)
+  console.log(
+    'next check scheduled for',
+    Math.round(rand / 1000 / 60),
+    'minutes'
+  )
+  setTimeout(function () {
+    checkForAppts()
+    loop()
+  }, rand)
 })()

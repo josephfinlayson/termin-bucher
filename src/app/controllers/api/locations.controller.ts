7428c34c4508c261eb9c32752d297d9985b18f42
Joseph Finlayson <joseph.finlayson@gmail.com>
2021-03-01 11:14:31 +0100

    fix locations query

--- src/app/controllers/api/locations.controller.ts
+++ src/app/controllers/api/locations.controller.ts
@@ -1,16 +1,24 @@
+/* eslint-disable @typescript-eslint/camelcase */
 import { Context, Get, HttpResponseOK } from '@foal/core';
 import {Location} from '../../entities'
-import {getRepository} from "typeorm"
+import {getRepository} from 'typeorm'
 export class LocationsController {
 
   @Get('/')
   async get(ctx: Context) {
     const rep = getRepository(Location)
-    const locs = await rep.find()
-    const locations = locs.map((row) => {
+
+    const locs = await rep.createQueryBuilder()
+    .distinctOn(['authority_id'])
+    .select(['authority_name', 'authority_id'])
+    .getRawMany()
+    
+    console.log(locs)
+    
+    const locations = locs.map(row => {
         return {
-            authority_name: row.authorityName.replace('Bezirksamt ', ''),
-            authority_id: row.authorityId
+            authority_name: row.authority_name.replace('Bezirksamt ', ''),
+            authority_id: row.authority_id
         }
     })
 

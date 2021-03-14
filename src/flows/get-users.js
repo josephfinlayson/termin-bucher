1884b2380bf8f224d1c6129789e6392f7cdc2abf
Joseph Finlayson <joseph.finlayson@gmail.com>
2021-03-14 19:05:47 +0100

    fix query

--- src/flows/get-users.js
+++ src/flows/get-users.js
@@ -4,32 +4,20 @@ import knex from '../data/index'
 
 const moment = extendMoment(mom)
 
-export default async function getUsersRegisteredInLast7Days () {
-  
-    console.log(knex('users')
-    .whereBetween('created_at', [
-      moment()
-        .subtract(7, 'days').format('YYYY-MM-DD HH:mm:ss'),
-      moment().format('YYYY-MM-DD HH:mm:ss')
-    ])
-    .andWhere({ appointment_found: false })
-    .joinRaw('join location_user_mapping on location_user_mapping.user_id = users.id')
-    .andWhereRaw('authority_id IS NOT NULL')
-    .select('*')
-    .limit(1).toQuery())
+export default async function getUsersRegisteredInLast7Days() {
     try {
-    return await knex('users')
-      .whereBetween('created_at', [
-        moment()
-          .subtract(7, 'days').format('YYYY-MM-DD HH:mm:ss'),
-        moment().format('YYYY-MM-DD HH:mm:ss')
-      ])
-      .andWhere({ appointment_found: false })
-      .joinRaw('join location_user_mapping on location_user_mapping.user_id = users.id')
-      .andWhereRaw('authority_id IS NOT NULL')
-      .select('*')
-      .limit(1)
-  } catch (e) {
-    console.error('cannot get users', e)
-  }
+        return await knex('users')
+            .whereBetween('created_at', [
+                moment()
+                .subtract(7, 'days').format('YYYY-MM-DD HH:mm:ss'),
+                moment().format('YYYY-MM-DD HH:mm:ss')
+            ])
+            .andWhere({ appointment_found: false })
+            .joinRaw('join location_user_mapping on location_user_mapping.user_id = users.id')
+            .andWhereRaw('authority_id IS NOT NULL')
+            .select('users.id')
+            .limit(1)
+    } catch (e) {
+        console.error('cannot get users', e)
+    }
 }
